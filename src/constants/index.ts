c6d5730cc93258993034a0b58b9e586b1fbd1465
andrew stebenne <arthropodSeven@protonmail.com>
2022-02-27 18:24:36 -0500

    add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,30 @@
+
+export const alphabet = [
+  "Alpha",
+  "Bravo",
+  "Charlie",
+  "Delta",
+  "Echo",
+  "Foxtrot",
+  "Golf",
+  "Hotel",
+  "India",
+  "Juliett",
+  "Kilo",
+  "Lima",
+  "Mike",
+  "November",
+  "Oscar",
+  "Papa",
+  "Quebec",
+  "Romeo",
+  "Sierra",
+  "Tango",
+  "Uniform",
+  "Victor",
+  "Whiskey",
+  "Xray",
+  "Yankee",
+  "Zulu"
+];
+
