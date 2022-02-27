7612051218ea9f1f85da5f614bef9a1252aea824
andrew stebenne <arthropodSeven@protonmail.com>
2022-02-27 14:23:42 -0500

    prettier fix

--- src/layouts/Home/Home.tsx
+++ src/layouts/Home/Home.tsx
@@ -8,17 +8,12 @@ const HomeRoot = styled.div`
   display: grid;
   grid-gap: 1ch;
   grid-auto-rows: auto;
-  grid-template-columns:
-    minmax(0, 1fr) minmax(min-content, 80ch) minmax(0, 1fr);
+  grid-template-columns: minmax(0, 1fr) minmax(min-content, 80ch) minmax(0, 1fr);
   & > * {
     grid-column: 2 / -2;
   }
 `;
 
 export const Home: FC<HomeProps> = (props) => {
-  return (
-    <HomeRoot>
-      {props.children}
-    </HomeRoot>
-  );
+  return <HomeRoot>{props.children}</HomeRoot>;
 };
