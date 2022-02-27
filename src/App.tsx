b89e28d526f5a73dd05e462280e35f3649bd34a9
andrew stebenne <arthropodSeven@protonmail.com>
2022-02-27 14:23:30 -0500

    use Home layout on homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import { ThemeProvider } from "styled-components";
 import { Text } from "./components";
+import { Home } from "./layouts";
 import theme from "./theme";
 
 type AppProps = {
@@ -10,7 +11,9 @@ type AppProps = {
 export const App: FC<AppProps> = () => {
   return (
     <ThemeProvider theme={theme.light}>
-      <Text>Let's put the posts here.</Text>
+      <Home>
+        <Text>Let's put the posts here.</Text>
+      </Home>
     </ThemeProvider>
   );
 };
