66cfe2b76fdceb967baf361d75560ee397098d2d
andrew stebenne <arthropodSeven@protonmail.com>
2022-03-01 01:10:14 -0500

    add utils with makePatp

---
+++ src/utilities/index.ts
@@ -0,0 +1,14 @@
+import { prefixes, suffixes } from "@tlon/phonemes-js";
+
+// takes four arbitrary numbers of ascending magnitude, represented as two
+// arrays of two numbers, and constructs an @p
+export const makePatp = (syllables: Array<Array<number>>) => {
+  syllables
+  .map((pair) => {
+    const [pre, suf] = pair.map(n => n % 256);
+    return [
+      prefixes[pre],
+      suffixes[suf],
+    ].join('');
+  }).join('-');
+};
