f112c260b9f60f643b8169d31a397e4617e7bd33
andrew stebenne <arthropodSeven@protonmail.com>
2022-03-01 02:17:04 -0500

    flex without box

--- src/components/Flex/Flex.tsx
+++ src/components/Flex/Flex.tsx
@@ -1,10 +1,9 @@
 import styled from "styled-components";
 import { flexbox, FlexboxProps } from "styled-system";
-import { Box } from "../Box";
 
 export type FlexProps = FlexboxProps & { gap?: number };
 
-export const Flex: any = styled(Box)<FlexProps>(
+export const Flex: any = styled.div<FlexProps>(
   {
     display: "flex",
     gap: (props) => props.gap,
