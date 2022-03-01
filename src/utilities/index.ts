ad387e31b07fd42c4c407fb45858515bf6bbd96b
andrew stebenne <arthropodSeven@protonmail.com>
2022-03-01 13:53:20 -0500

    update makePatp signature

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -2,7 +2,7 @@ import { prefixes, suffixes } from "@tlon/phonemes-js";
 
 // takes four arbitrary numbers of ascending magnitude, represented as two
 // arrays of two numbers, and constructs an @p
-export const makePatp = (syllables: Array<Array<number>>) => {
+export const makePatp = (syllables: [[number, number], [number, number]]) => {
   return syllables
     .map((pair) => {
       const [pre, suf] = pair.map((n) => n % 256);
