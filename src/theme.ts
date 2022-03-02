097f0fd58c9178cc6c5086c0fc8c20dd70bb37ec
andrew stebenne <arthropodSeven@protonmail.com>
2022-03-01 19:28:18 -0500

    relative line heights

--- src/theme.ts
+++ src/theme.ts
@@ -37,9 +37,9 @@ export const fontWeights = {
 };
 
 export const lineHeights = {
-  solid: "1rem",
-  title: "1.25rem",
-  copy: "1.5rem",
+  solid: 1,
+  title: 1.25,
+  copy: 1.5,
 };
 
 export const letterSpacings = {
